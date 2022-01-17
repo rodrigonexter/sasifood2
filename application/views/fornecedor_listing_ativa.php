1f27b4aaa54551b28f6eb8c7cf483f59bcc82c6c
rodrigo <rodrigodeveloper96@gmail.com>
2022-01-17 09:28:02 -0300

    v2

--- application/views/fornecedor_listing_ativa.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	
-	echo heading($headline, 2);
-	echo br();
-	echo $data_table; 
-
-/* End of file fornecedor_listing.php */
-/* Location: ./system/application/views/fornecedor_listing.php */
--- application/views/usuario_listing.php
+++
@@ -1,7 +0,0 @@
-﻿<?php 
-
-	echo heading($headline, 2);
-	echo $data_table; 
-	
-/* End of file usuario_listing.php */
-/* Location: ./application/controllers/usuario_listing.php */
